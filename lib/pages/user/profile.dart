acbcd98094999facc4a87307006bd32182cab1c0
muhammadfawwazarshadsaid <muhammad.fawwaz22@ui.ac.id>
2023-12-04 14:19:54 +0700

    UI upvote wishlist done

--- lib/pages/user/profile.dart
+++ lib/pages/user/profile.dart
@@ -2,16 +2,18 @@
 
 import 'package:flutter/material.dart';
 import 'package:litracker_mobile/pages/loginPage.dart';
+import 'package:litracker_mobile/pages/user/upvoteList.dart';
+import 'package:litracker_mobile/pages/user/wishlistList.dart';
 
 class ProfileContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Stack(
-        alignment: Alignment.bottomCenter,
+        alignment: Alignment.center,
         children: [
           Transform.scale(
-            scale: 1.5,
+            scale: 2.0,
             child: Image.asset("assets/profile/profile-bg.png"),
           ),
           Container(
@@ -113,115 +115,137 @@ class ProfileContent extends StatelessWidget {
                   ),
                   child: Column(
                     children: [
-                      Container(
-                        padding: EdgeInsets.all(12),
-                        decoration: BoxDecoration(
-                          color: const Color.fromRGBO(246, 247, 249, 1),
-                          borderRadius: BorderRadius.circular(24),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Image.asset(
-                                    'assets/profile/profile-upvote.png'),
-                                SizedBox(width: 16),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    RichText(
-                                      text: const TextSpan(
-                                        text: "1604",
-                                        style: TextStyle(
-                                          fontFamily: 'SF-Pro',
-                                          fontWeight: FontWeight.w700,
-                                          color: Color.fromRGBO(8, 4, 22, 1),
-                                          fontSize: 16,
-                                        ),
-                                      ),
-                                    ),
-                                    SizedBox(height: 8),
-                                    RichText(
-                                      text: const TextSpan(
-                                        text: "Buku Kamu Upvote",
-                                        style: TextStyle(
-                                          fontFamily: 'SF-Pro',
-                                          fontWeight: FontWeight.w400,
-                                          color:
-                                              Color.fromRGBO(132, 151, 172, 1),
-                                          fontSize: 12,
-                                          letterSpacing: -0.7,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => UpVoteList(),
                             ),
-                            SizedBox(width: 16),
-                            Image.asset('assets/profile/profile-directto.png'),
-                          ],
+                          );
+                        },
+                        child: Container(
+                          padding: EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: const Color.fromRGBO(246, 247, 249, 1),
+                            borderRadius: BorderRadius.circular(24),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Image.asset(
+                                      'assets/profile/profile-upvote.png'),
+                                  SizedBox(width: 16),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      RichText(
+                                        text: const TextSpan(
+                                          text: "1604",
+                                          style: TextStyle(
+                                            fontFamily: 'SF-Pro',
+                                            fontWeight: FontWeight.w700,
+                                            color: Color.fromRGBO(8, 4, 22, 1),
+                                            fontSize: 16,
+                                          ),
+                                        ),
+                                      ),
+                                      SizedBox(height: 8),
+                                      RichText(
+                                        text: const TextSpan(
+                                          text: "Buku Kamu Upvote",
+                                          style: TextStyle(
+                                            fontFamily: 'SF-Pro',
+                                            fontWeight: FontWeight.w400,
+                                            color: Color.fromRGBO(
+                                                132, 151, 172, 1),
+                                            fontSize: 12,
+                                            letterSpacing: -0.7,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              SizedBox(width: 16),
+                              Image.asset(
+                                  'assets/profile/profile-directto.png'),
+                            ],
+                          ),
                         ),
                       ),
-                      SizedBox(
-                        height: 24,
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(12),
-                        decoration: BoxDecoration(
-                          color: const Color.fromRGBO(246, 247, 249, 1),
-                          borderRadius: BorderRadius.circular(24),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Image.asset(
-                                    'assets/profile/profile-wishlist.png'),
-                                SizedBox(width: 16),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    RichText(
-                                      text: const TextSpan(
-                                        text: "1604",
-                                        style: TextStyle(
-                                          fontFamily: 'SF-Pro',
-                                          fontWeight: FontWeight.w700,
-                                          color: Color.fromRGBO(8, 4, 22, 1),
-                                          fontSize: 16,
-                                        ),
-                                      ),
-                                    ),
-                                    SizedBox(height: 8),
-                                    RichText(
-                                      text: const TextSpan(
-                                        text: "Buku di Wishlistmu",
-                                        style: TextStyle(
-                                          fontFamily: 'SF-Pro',
-                                          fontWeight: FontWeight.w400,
-                                          color:
-                                              Color.fromRGBO(132, 151, 172, 1),
-                                          fontSize: 12,
-                                          letterSpacing: -0.7,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
+                      SizedBox(height: 24),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => WishlistList(),
                             ),
-                            SizedBox(width: 16),
-                            Image.asset('assets/profile/profile-directto.png'),
-                          ],
+                          );
+                        },
+                        child: Container(
+                          padding: EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: const Color.fromRGBO(246, 247, 249, 1),
+                            borderRadius: BorderRadius.circular(24),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Image.asset(
+                                      'assets/profile/profile-wishlist.png'),
+                                  SizedBox(width: 16),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      RichText(
+                                        text: const TextSpan(
+                                          text: "1604",
+                                          style: TextStyle(
+                                            fontFamily: 'SF-Pro',
+                                            fontWeight: FontWeight.w700,
+                                            color: Color.fromRGBO(8, 4, 22, 1),
+                                            fontSize: 16,
+                                          ),
+                                        ),
+                                      ),
+                                      SizedBox(height: 8),
+                                      RichText(
+                                        text: const TextSpan(
+                                          text: "Buku di Wishlistmu",
+                                          style: TextStyle(
+                                            fontFamily: 'SF-Pro',
+                                            fontWeight: FontWeight.w400,
+                                            color: Color.fromRGBO(
+                                                132, 151, 172, 1),
+                                            fontSize: 12,
+                                            letterSpacing: -0.7,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              SizedBox(width: 16),
+                              Image.asset(
+                                  'assets/profile/profile-directto.png'),
+                            ],
+                          ),
                         ),
                       ),
                     ],
@@ -236,17 +260,18 @@ class ProfileContent extends StatelessWidget {
                     GestureDetector(
                       onTap: () {
                         // Navigate to LoginPage here
-                        Navigator.push(
+                        Navigator.pushReplacement(
                           context,
                           MaterialPageRoute(
                             builder: (context) => LoginPage(),
                           ),
                         );
+                        Navigator.pop(context);
                       },
                       child: Container(
                         padding: EdgeInsets.symmetric(
                           vertical: 12,
-                          horizontal: 24,
+                          horizontal: 22,
                         ),
                         alignment: Alignment.centerRight,
                         height: 52,
@@ -254,6 +279,10 @@ class ProfileContent extends StatelessWidget {
                         decoration: BoxDecoration(
                           color: Colors.white,
                           borderRadius: BorderRadius.circular(20),
+                          border: Border.all(
+                            color: Color.fromRGBO(236, 239, 242, 1),
+                            width: 2,
+                          ),
                         ),
                         child: Row(
                           children: [
